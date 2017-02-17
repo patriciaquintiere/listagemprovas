a9e128f27b4d6c24e524227ed4d60edab5013130
Patrícia Quintiere <patricia@patlu-aviador.luizepatricia>
2017-02-17 01:19:14 -0200

    Filtros Funcionando

--- src/app/app.js
+++ src/app/app.js
@@ -1,262 +1,54 @@
 var app = angular.module('app', []);
 app.controller('appController',['$scope','$http','$location', function($scope, $http, $location){
 
-    $scope.simulados = [
-        {
-            nome: 'simulado 10',
-            provas: [
-                {
-                    tipo: 'objetiva',
-                    itens: [
-                        {
-                            prova:
-                                {
-                                    matricula: 11111,
-                                    posicao: 1,
-                                    nota: 40,
-                                    especialidade: 'clínica médica',
-                                    turma: '2008 MEDGRUPO SALVADOR - Turma A/2008 SALVADOR',
-                                    filial: 'Salvador(BA)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 22222,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'São Paulo(SP)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 333333,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'Salvador(BA)'
-                                }
-                        }
-                    ]
-                },
-                {
-                    tipo: 'final',
-                    itens: [
-                        {
-                            prova:
-                                {
-                                    matricula: 11111,
-                                    posicao: 1,
-                                    nota: 40,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO RIO DE JANEIRO - Turma A/2008 RIO DE JANEIRO',
-                                    filial: 'Rio de Janeiro(BA)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 22222,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'São Paulo(SP)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 333333,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica médica',
-                                    turma: '2008 MEDGRUPO SALVADOR PAULO - Turma A/2008 SALVADOR',
-                                    filial: 'Salvador(BA)'
-                                }
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            nome: 'simulado 9',
-            provas: [
-                {
-                    tipo: 'objetiva',
-                    itens: [
-                        {
-                            prova:
-                                {
-                                    matricula: 44444,
-                                    posicao: 1,
-                                    nota: 40,
-                                    especialidade: 'clínica médica',
-                                    turma: '2008 MEDGRUPO SALVADOR - Turma A/2008 SALVADOR',
-                                    filial: 'Salvador(BA)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 11111,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'São Paulo(SP)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 77777,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'Salvador(BA)'
-                                }
-                        }
-                    ]
-                },
-                {
-                    tipo: 'final',
-                    itens: [
-                        {
-                            prova:
-                                {
-                                    matricula: 11111,
-                                    posicao: 1,
-                                    nota: 40,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO RIO DE JANEIRO - Turma A/2008 RIO DE JANEIRO',
-                                    filial: 'Rio de Janeiro(BA)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 22222,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'São Paulo(SP)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 333333,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica médica',
-                                    turma: '2008 MEDGRUPO SALVADOR PAULO - Turma A/2008 SALVADOR',
-                                    filial: 'Salvador(BA)'
-                                }
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            nome: 'simulado 8',
-            provas: [
-                {
-                    tipo: 'objetiva',
-                    itens: [
-                        {
-                            prova:
-                                {
-                                    matricula: 33333,
-                                    posicao: 1,
-                                    nota: 40,
-                                    especialidade: 'clínica médica',
-                                    turma: '2008 MEDGRUPO SALVADOR - Turma A/2008 SALVADOR',
-                                    filial: 'Salvador(BA)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 11111,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'São Paulo(SP)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 222222,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'Salvador(BA)'
-                                }
-                        }
-                    ]
-                },
-                {
-                    tipo: 'final',
-                    itens: [
-                        {
-                            prova:
-                                {
-                                    matricula: 11111,
-                                    posicao: 1,
-                                    nota: 40,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO RIO DE JANEIRO - Turma A/2008 RIO DE JANEIRO',
-                                    filial: 'Rio de Janeiro(BA)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 22222,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica geral',
-                                    turma: '2008 MEDGRUPO SÃO PAULO - Turma A/2008 SÃO PAULO',
-                                    filial: 'São Paulo(SP)'
-                                }
-                        },
-                        {
-                            prova:
-                                {
-                                    matricula: 333333,
-                                    posicao: 3,
-                                    nota: 76,
-                                    especialidade: 'clínica médica',
-                                    turma: '2008 MEDGRUPO SALVADOR PAULO - Turma A/2008 SALVADOR',
-                                    filial: 'Salvador(BA)'
-                                }
-                        }
-                    ]
-                }
-            ]
-        },
-
-    ];
-
-
-
-    $scope.tipoNum = '';
-    $scope.tipoProva = '';
-
-    $scope.tipoSimulado = function(simuladorNum,simuladorProva){
-        $scope.tipoNum = simuladorNum,
-        $scope.tipoProva = simuladorProva
+    // principais filtros
+    $scope.search = {
+        simulado: 2,
+        tipo: 'objetiva'
     };
+
+    /////////// MENU
+    // lista simulados no menu
+    var listaMenu = function(){
+        $http ({
+            method: 'GET',
+            url: 'app/data/listasimulados.json'
+        }).then(function (response){
+            $scope.simulados = response.data;
+
+        }).catch (function (response){
+            if(response.status == 404){
+            }
+        });
+    };
+
+    $scope.tipos = '';
+    // salva no click tipo da prova
+    $scope.getSimuladoByTipo = function(itemSimulado,tipo){
+        $scope.search.simulado = itemSimulado;
+        $scope.search.tipo = tipo;
+    };
+
+    /////////// FIM MENU
+
+    /////////// LISTA TABELA PROVAS
+    var listaProva = function() {
+        $http ({
+            method: 'GET',
+            url: 'app/data/listaprovas.json'
+        }).then(function (response){
+            $scope.provas = response.data;
+        }).catch (function (response){
+            console.log('deu merda');
+            console.log(response);
+            if(response.status == 404){
+                console.log('deu erro');
+            }
+        });
+    };
+    /////////// LISTA TABELA PROVAS
+
+
 
     // botoes do tamanho da fonte da pagina
     $scope.fontsizecurrent = 1.0;
@@ -278,96 +70,8 @@ app.controller('appController',['$scope','$http','$location', function($scope, $
     };
 
 
-
-    // // MSG DE ERRO
-    // $scope.msg = {
-    //     orgError: 'Empresa não localizada. Verifique o campo e digite novamente.',
-    //     userError: 'Usuário não localizado. Verifique o campo e digite novamente.',
-    //     viewOrgError: '',
-    //     viewUserError: ''
-    // };
-    // // inputs
-    // $scope.search = {
-    //     orgname: '',
-    //     username: '',
-    //     filterOrg: ''
-    // };
-    //
-    // //// MOSTRA OU ESCONDE BLOCO
-    // //// QUANDO RETORNADO O OBJETO COM LISTA DE REPOSITORIOS
-    // // LISTA EMPRESA
-    // $scope.infoRepOrg = {
-    //     show: false
-    // };
-    // // LISTA USUARIO
-    // $scope.infoRepUser = {
-    //     show: false
-    // };
-    //
-    // // LISTA REPOSITORIOS
-    // $scope.reposit = {
-    //     listOrg: '',
-    //     listUser: '',
-    // };
-    //
-    // // BUSCA REPOSITORIO EMPRESA
-    // $scope.buscaRepositorioEmpresas = function() {
-    //     $http ({
-    //         method: 'GET',
-    //         url: 'https://api.github.com/orgs/'+$scope.search.orgname+'/repos'
-    //     }).then(function (response){
-    //         // mostra html da lista informacoes de repositorios
-    //         $scope.infoRepOrg.show = true;
-    //         // passa dados da requisicao para variavel
-    //         $scope.reposit.listOrg = [];
-    //         $scope.reposit.listOrg = response.data;
-    //         if(response.data == 0){
-    //             $scope.msg.viewOrgError = 'Não há repositórios para esta empresa';
-    //         } else {
-    //             // remove msg de erro
-    //             $scope.msg.viewOrgError = '';
-    //         }
-    //     }).catch (function (response){
-    //         // esconde html da lista de informacao de repositorios
-    //         $scope.infoRepOrg.show  = false;
-    //         // limpa variavel - lista de repositorios
-    //         $scope.reposit.listOrg = '';
-    //         // mensagem de erro
-    //         if(response.status == 404){
-    //             $scope.infoRepOrg.show = true;
-    //             $scope.msg.viewOrgError = $scope.msg.orgError;
-    //         }
-    //     });
-    // };
-    //
-    // // BUSCA REPOSITORIO USUARIOS
-    // $scope.buscaRepositorioUsuarios = function() {
-    //     $http ({
-    //         method: 'GET',
-    //         url: 'https://api.github.com/users/'+$scope.search.username+'/repos'
-    //     }).then(function (response){
-    //         // mostra html da lista informacoes de repositorios
-    //         $scope.infoRepUser.show = true;
-    //         // passa dados da requisicao para variavel
-    //         $scope.reposit.listUser = [];
-    //         $scope.reposit.listUser = response.data;
-    //         if(response.data == 0){
-    //             $scope.msg.viewUserError = 'Não há repositórios para este usuário';
-    //         } else {
-    //             // remove msg de erro
-    //             $scope.msg.viewUserError = '';
-    //         }
-    //     }).catch (function (response){
-    //         // esconde html da lista de informacao de repositorios
-    //         $scope.infoRepUser.show = false;
-    //         // limpa variavel - lista de repositorios
-    //         $scope.reposit.listUser = '';
-    //         if(response.status == 404){
-    //             $scope.infoRepUser.show = true;
-    //             $scope.msg.viewUserError = $scope.msg.userError;
-    //         }
-    //     });
-    // };
-
+    // inicializando algumas funcoes
+    listaProva();
+    listaMenu();
 
 }]);
--- src/app/table/table.component.js
+++
@@ -1,5 +0,0 @@
-app.component('tableSimulado', {
-  // Note: The URL is relative to our `index.html` file
-  templateUrl: 'app/table/table.template.html',
-  controller: 'appController'
-});
